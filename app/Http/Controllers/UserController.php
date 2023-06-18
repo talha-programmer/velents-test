733f577c2b28d3831c8633db3b7af872948aa3c6
Muhammad Talha <talhashabbir9@gmail.com>
2023-06-19 02:45:05 +0500

    error handling

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Resources\UserResource;
 use App\Models\User;
+use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Storage;
@@ -21,73 +22,86 @@ class UserController extends Controller
 
     public function store(Request $request)
     {
-        $request->validate([
-            'first_name' => 'required|string',
-            'last_name' => 'required|string',
-            'email' => 'required|unique:users,email',
-            'password' => 'required|string',
-            'photo' => 'nullable|image'
-        ]);
+        try {
+            $request->validate([
+                'first_name' => 'required|string',
+                'last_name' => 'required|string',
+                'email' => 'required|unique:users,email',
+                'password' => 'required|string',
+                'photo' => 'nullable|image'
+            ]);
 
-        $photoUrl = $request->photo ? $request->file('photo')->store('photos') : null;
-        $password = Hash::make($request->password);
+            $photoUrl = $request->photo ? $request->file('photo')->store('photos') : null;
+            $password = Hash::make($request->password);
 
-        $user = User::create([
-            ...$request->only(['first_name', 'last_name', 'email']),
-            'password' => $password,
-            'photo' => $photoUrl,
-        ]);
+            $user = User::create([
+                ...$request->only(['first_name', 'last_name', 'email']),
+                'password' => $password,
+                'photo' => $photoUrl,
+            ]);
 
-        $user = new UserResource($user);
+            $user = new UserResource($user);
 
-        $response = formatResponse(['user' => $user], true, 'User saved successfully');
-        return response()->json($response, 201);
+            $response = formatResponse(['user' => $user], true, 'User saved successfully');
+            return response()->json($response, 201);
+        } catch (Exception $e) {
+            return response()->json(formatResponse(null, false, $e->getMessage()), 500);
+        }
     }
 
 
     public function update(Request $request, User $user)
     {
-         $request->validate([
-            'first_name' => 'nullable|string',
-            'last_name' => 'nullable|string',
-            'password' => 'nullable|string',
-            'email' => ["nullable", Rule::unique('users')->ignore($user->id)],
-            'photo' => 'nullable|image'
-        ]);
+        try {
+            $request->validate([
+                'first_name' => 'nullable|string',
+                'last_name' => 'nullable|string',
+                'password' => 'nullable|string',
+                'email' => ["nullable", Rule::unique('users')->ignore($user->id)],
+                'photo' => 'nullable|image'
+            ]);
 
-        $photoUrl = null;
-        if($request->photo){
-            Storage::delete("photos/{$user->photo}");
-            $photoUrl = $request->file('photo')->store('photos');
+            $photoUrl = null;
+            if ($request->photo) {
+                Storage::delete("photos/{$user->photo}");
+                $photoUrl = $request->file('photo')->store('photos');
+            }
+
+            $password = $request->password ? Hash::make($request->password) : null;
+
+            $values = [
+                ...$request->only(['first_name', 'last_name', 'email']),
+                'password' => $password,
+                'photo' => $photoUrl,
+            ];
+
+            $values = array_filter($values, fn ($item) => !empty($item));
+
+            $user->update($values);
+            $user = new UserResource($user);
+
+            $response = formatResponse(['user' => $user], true, 'User updated successfully');
+            return response()->json($response);
+        } catch (Exception $e) {
+            return response()->json(formatResponse(null, false, $e->getMessage()), 500);
         }
-
-        $password = $request->password ? Hash::make($request->password): null;
-
-        $values = [
-            ...$request->only(['first_name', 'last_name', 'email']),
-            'password' => $password,
-            'photo' => $photoUrl,
-        ];
-
-        $values = array_filter($values, fn($item) => !empty($item));
-
-        $user->update($values);
-        $user = new UserResource($user);
-        
-        $response = formatResponse(['user' => $user], true, 'User updated successfully');
-        return response()->json($response);
     }
 
 
     public function destroy(User $user)
     {
-        if(is_file($user->photo)){
-            Storage::delete("photos/{$user->photo}");
+        try {
+            if (is_file($user->photo)) {
+                Storage::delete("photos/{$user->photo}");
+            }
+
+            $user->delete();
+
+            $response = formatResponse(null, true, 'User deleted successfully');
+            return response()->json($response);
+        } catch (Exception $e) {
+
+            return response()->json(formatResponse(null, false, $e->getMessage()), 500);
         }
-
-        $user->delete();
-
-        $response = formatResponse(null, true, 'User deleted successfully');
-        return response()->json($response);
     }
 }
